0e7c632a1b4c83b8cc52bcce91122bcdc5976204
syavira08 <syavira.firnanda018@gmail.com>
2026-09-03 13:47:01 +0800

    Update Pertemuan2.java

--- Pertemuan2/src/main/java/com/mycompany/pertemuan2/Pertemuan2.java
+++ Pertemuan2/src/main/java/com/mycompany/pertemuan2/Pertemuan2.java
@@ -11,6 +11,6 @@ package com.mycompany.pertemuan2;
 public class Pertemuan2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Laper");
     }
 }
